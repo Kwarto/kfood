d6f276ee8b020ca947c2871aa57f424d3236ba8c
Kwarto <dbitsolu@gmail.com>
2023-01-12 14:45:03 +0000

    Added p tag to new change to all

--- src/components/AllKitchens.jsx
+++ src/components/AllKitchens.jsx
@@ -4,8 +4,9 @@ const AllKitchens = () => {
   return (
     <section className='w-full min-h-[80vh] py-4 md:px-16 px-4'>
       <h1 className='font-bold md:text-[2rem] text-[1.5rem] text-headingColor py-2'>Available Kitchens</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, corporis optio earum dolorem perspiciatis provident dolorum ad modi architecto in.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, corporis optio earum dolorem perspiciatis provident dolorum ad modi architecto in.</p>
     </section>
+    
   )
 }
 
